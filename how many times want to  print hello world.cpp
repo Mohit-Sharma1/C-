79192593d47acfe6a81ccd08e5f0a017cf4a2b49
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-08-21 23:42:56 +0530

    Add files via upload

---
+++ how many times want to  print hello world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ int c,n;
+ cout<<"How many times wants to print hello world?\n"<<endl;
+ cin>>n;
+ for  (c=1; c<=n; c++)
+ 
+ 	cout<<"Hello World\n"<<endl;
+ 
+}
+
