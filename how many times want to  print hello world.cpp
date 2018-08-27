b1ee581d87a6d7b9a934023f17cb8dc727cc3e34
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-08-27 17:34:06 +0530

    Update how many times want to  print hello world.cpp

--- how many times want to  print hello world.cpp
+++ how many times want to  print hello world.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include<iostream>       //Headerfile
 using namespace std;
-main()
+main()                   //Program start
 {
  int c,n;
- cout<<"How many times wants to print hello world?\n"<<endl;
- cin>>n;
- for  (c=1; c<=n; c++)
+ cout<<"How many times wants to print hello world?\n"<<endl;           //Display the output
+ cin>>n;   //Getting data from users
+ for  (c=1; c<=n; c++)     //Loop starts
  
- 	cout<<"Hello World\n"<<endl;
+ 	cout<<"Hello World\n"<<endl;       //Display the output
  
 }
 
