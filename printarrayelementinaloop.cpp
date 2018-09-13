1bbffeabcbbf3b60d6ceb32748f5da4ac9123de0
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-09-13 23:38:52 +0530

    Add files via upload

---
+++ printarrayelementinaloop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num[5],i,j;
+    cout<<"enter elements"<<endl;
+   
+	
+    for (i=0;i<5;i++)
+    {
+    	
+    	cin>>num[i];
+    //out<<Num<<""<<endl;
+	}
+	if (num[5]==num[5] )
+	
+	for(j=0;i<=19;i++){
+	
+	for (i=0;i<5;i++){
+		
+	
+	cout<<num[i]<<" ";
+}
+}
+	
+    
+    return 0;
+}
