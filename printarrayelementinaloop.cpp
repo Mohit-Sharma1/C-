05cf5f061668c24a920c5b7e9dd5e746c96f4fcb
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-09-14 15:18:46 +0530

    Update printarrayelementinaloop.cpp

--- printarrayelementinaloop.cpp
+++ printarrayelementinaloop.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
+#include <iostream>                       //Headerfile Used
 using namespace std;
 
-int main() 
+int main()                              //Program starts
 {
     int num[5],i,j;
-    cout<<"enter elements"<<endl;
+    cout<<"enter elements"<<endl;       //To Display the elements
    
 	
-    for (i=0;i<5;i++)
+    for (i=0;i<5;i++)                   //Loop Used 
     {
     	
-    	cin>>num[i];
-    //out<<Num<<""<<endl;
+    	cin>>num[i];                  //Add number by users
 	}
-	if (num[5]==num[5] )
+	if (num[5]==num[5] )           //Conditional element used
 	
-	for(j=0;i<=19;i++){
+	for(j=0;j<=8;i++){              //loop  used
 	
 	for (i=0;i<5;i++){
 		
 	
-	cout<<num[i]<<" ";
+	cout<<num[i]<<" ";               //Display element
 }
 }
 	
