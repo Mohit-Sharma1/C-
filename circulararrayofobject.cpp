ce1a5f2488fe1953e9eb225a57b81c70b0228275
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-10-04 23:23:18 +0530

    Add files via upload

---
+++ circulararrayofobject.cpp
@@ -0,0 +1,30 @@
+// CPP program to demonstrate use of circular array
+#include <iostream>
+using namespace std;
+class play{
+	public:
+
+void print(char a[], int n, int ind)
+{
+    // Create an auxiliary array of twice size.
+    char b[(2 * n)];
+ 
+    // Copy a[] to b[] two times 
+    for (int i = 0; i < n; i++) 
+        b[i] = b[n + i] = a[i];
+ 
+    // print from ind-th index to (n+i)th index.
+    for (int i = ind; i < n + ind; i++)
+        cout << b[i] << " ";
+}};
+ 
+// driver code 
+int main()
+{
+	play p;
+	 char a[] = { 'A', 'B', 'C', 'D', 'E', 'F','G' };
+    int n = sizeof(a) / sizeof(a[0]);
+    p.print(a, n, 3);
+     return 0;
+}
+
